71a300d1c6aaeccc7efe5dde410035ae0d91ffae
Sneha Agrawal <agrawal.sneha1434@gmail.com>
2024-04-17 19:16:28 +0530

    Time: 3 ms (24.91%), Space: 7 MB (99.11%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        
+        string ans;
+        for(auto i: address){
+            if(i=='.'){
+                ans+="[.]";
+            }
+            else{
+                ans+=i;
+            }
+        }
+        
+        return ans;
+        
+    }
+};
