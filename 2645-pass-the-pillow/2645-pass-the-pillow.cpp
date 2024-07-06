fea37a60e90740c1a642c132be22da0aec2c380f
Sneha Agrawal <agrawal.sneha1434@gmail.com>
2024-07-06 18:02:04 +0530

    Time: 0 ms (100%), Space: 7.1 MB (71.18%) - LeetHub

--- 2645-pass-the-pillow/2645-pass-the-pillow.cpp
+++ 2645-pass-the-pillow/2645-pass-the-pillow.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int passThePillow(int n, int time) {
         
-        if(n > time){
-            return time+1;
-        }
+        // if(n > time){
+        //     return time+1;
+        // }
         bool rl = (time/(n-1))%2;
         int pos = time%(n-1);
         if(rl)
